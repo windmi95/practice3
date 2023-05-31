bfeed41a0c7d0035698f76e0164038722de7c810
LMH <windmi95@nate.com>
2023-05-31 15:21:59 +0900

    resolve

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //홍길동 씨 평균 점수 구하기
+        //국어 = 80, 영어 = 75, 수학 = 55
+        /*
+        int korean = 80;
+        int english = 75;
+        int math = 55;
+        int total = korean+english+math;
+        float result = total/3;
+        System.out.println(result);
+
+        홀수, 짝수 판별
+        자연수 13이 홀수인지 짝수인지 판별
+        int num = 13;
+        boolean check = true;
+        check = 13%2==1;
+        System.out.println(check);
+
+
+
+
+
+
+
+
+    }
+}
